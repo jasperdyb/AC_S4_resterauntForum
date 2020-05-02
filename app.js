a5812b78beea237a64711c7c76c147e16ca2df36
Jasper Chen <jasperdyb@gmail.com>
2020-05-02 09:09:23 +0800

    Image upload function

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
+app.use('/upload', express.static(__dirname + '/upload')) //img url
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
