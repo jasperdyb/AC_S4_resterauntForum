634f23a58ba6a38a4008416c6c5f7e68c42310fc
Jasper Chen <jasperdyb@gmail.com>
2020-05-05 22:25:31 +0800

    Pagination

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,24 +1,38 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 12
 
 const restController = {
   getRestaurants: (req, res) => {
+    let offset = 0
     let whereQuery = {}
     let categoryId = ''
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery['CategoryId'] = categoryId
     }
 
-    Restaurant.findAll({
+    Restaurant.findAndCountAll({
       raw: true,
       nest: true,
       include: Category,
-      where: whereQuery
+      where: whereQuery,
+      offset: offset,
+      limit: pageLimit
     })
-      .then(restaurants => {
-        restaurants = restaurants.map(r => ({
+      .then(result => {
+        let currentPage = Number(req.query.page) || 1
+        let pages = Math.ceil(result.count / pageLimit)
+        let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+        let prev = currentPage - 1 < 1 ? 1 : currentPage - 1
+        let next = currentPage + 1 > pages ? pages : currentPage + 1
+
+        restaurants = result.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50),
           categoryName: r.Category.name
@@ -31,7 +45,11 @@ const restController = {
           return res.render('restaurants', {
             restaurants: restaurants,
             categories: categories,
-            categoryId: categoryId
+            categoryId: categoryId,
+            currentPage: currentPage,
+            totalPage: totalPage,
+            prev: prev,
+            next: next
           })
         })
       })
