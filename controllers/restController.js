2f47dbaed57cf86af37d7d71dd8dfa8b8d77d3f5
Jasper Chen <jasperdyb@gmail.com>
2020-05-05 22:09:49 +0800

    Catergory fliter

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,10 +4,18 @@ const Category = db.Category
 
 const restController = {
   getRestaurants: (req, res) => {
+    let whereQuery = {}
+    let categoryId = ''
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery['CategoryId'] = categoryId
+    }
+
     Restaurant.findAll({
       raw: true,
       nest: true,
-      include: Category
+      include: Category,
+      where: whereQuery
     })
       .then(restaurants => {
         restaurants = restaurants.map(r => ({
@@ -16,10 +24,30 @@ const restController = {
           categoryName: r.Category.name
         }))
 
-        return res.render('restaurants', {
-          restaurants: restaurants
+        Category.findAll({
+          raw: true,
+          nest: true
+        }).then(categories => { // 取出 categories 
+          return res.render('restaurants', {
+            restaurants: restaurants,
+            categories: categories,
+            categoryId: categoryId
+          })
         })
       })
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: Category
+    }).then(restaurant => {
+      console.log(restaurant)
+      return res.render('restaurant', {
+        restaurant: restaurant
+      })
+    })
   }
 }
 
