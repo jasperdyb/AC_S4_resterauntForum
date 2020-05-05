36296d9a5c8aa0d6eef9ca50cae67b4c0cb1c661
Jasper Chen <jasperdyb@gmail.com>
2020-05-05 19:50:28 +0800

    Show restaurants to user

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,26 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: Category
+    })
+      .then(restaurants => {
+        restaurants = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50),
+          categoryName: r.Category.name
+        }))
+
+        return res.render('restaurants', {
+          restaurants: restaurants
+        })
+      })
   }
 }
+
 module.exports = restController
